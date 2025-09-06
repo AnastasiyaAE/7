c21127178679ec46373cf0638afc2cd0e5f4f90b
AnastasiaE <naste@mail.ru>
2025-09-06 18:33:35 +0300

    Commit 5

---
+++ src/test/java/logincouriertest/Authorization.java
@@ -0,0 +1,11 @@
+package logincouriertest;
+
+public class Authorization {
+    private String login;
+    private String password;
+
+    public Authorization(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+}
--- src/test/java/logincouriertest/LoginHelper.java
+++ src/test/java/logincouriertest/LoginHelper.java
@@ -7,40 +7,40 @@ import io.restassured.response.Response;
 public class LoginHelper {
     @Step("Create courier with login: {login}, password: {password}, firstName: {firstName}")
     public Response createCourier(String login, String password, String firstName) {
-        String body = "{ \"login\": \"" + login + "\", \"password\": \"" + password + "\", \"firstName\": \"" + firstName + "\" }";
+        CourierCreate courier = new CourierCreate(login, password, firstName);
         return RestAssured.given()
                 .header("Content-Type", "application/json")
-                .body(body)
+                .body(courier)
                 .when()
                 .post("/api/v1/courier");
     }
 
     @Step("Login courier with login: {login}, password: {password}")
     public Response loginCourier(String login, String password) {
-    String body = "{ \"login\": \"" + login + "\", \"password\": \"" + password + "\" }";
+        Authorization authorization = new Authorization(login, password);
     return RestAssured.given()
             .header("Content-Type", "application/json")
-            .body(body)
+            .body(authorization)
             .when()
             .post("/api/v1/courier/login");
 }
 
     @Step("Login courier with missing login")
     public Response loginCourierWithMissingLogin(String password) {
-        String body = "{ \"password\": \"" + password + "\" }";
+        Authorization authorization = new Authorization(null, password);
         return RestAssured.given()
                 .header("Content-Type", "application/json")
-                .body(body)
+                .body(authorization)
                 .when()
                 .post("/api/v1/courier/login");
     }
 
     @Step("Login courier with missing password")
     public Response loginCourierWithMissingPassword(String login) {
-        String body = "{ \"login\": \"" + login + "\" }";
+        Authorization authorization = new Authorization(login, null);
         return RestAssured.given()
                 .header("Content-Type", "application/json")
-                .body(body)
+                .body(authorization)
                 .when()
                 .post("/api/v1/courier/login");
     }
